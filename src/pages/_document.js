ddf9902c7b5d71565a1d444e837999fa49b4fd97
kristiandavidd <kristiandavid644@gmail.com>
2024-10-17 23:56:30 +0700

    init

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="theme-color" content="#fff" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
