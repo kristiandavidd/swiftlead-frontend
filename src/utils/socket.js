0bfb974a996e5a58efe75f84defab7dae6b61bfd
kristiandavidd <kristiandavid644@gmail.com>
2024-10-22 23:55:13 +0700

    fix: api endpoint

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,7 +4,7 @@ let socket;
 
 export const initSocket = () => {
     if (!socket) {
-        socket = io('http://localhost:5000', {
+        socket = io('http://swiftlead-backend-production.up.railway.app', {
             path: '/socket.io',
             transports: ['websocket', 'polling'],
         });
