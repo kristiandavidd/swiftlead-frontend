df1a104a29659c89f1ff038ce5150cfd4f70dc44
kristiandavidd <kristiandavid644@gmail.com>
2025-01-11 14:44:35 +0700

    fix: cors

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -6,7 +6,7 @@ export const initSocket = () => {
 
     if (!socket) {
         const apiUrl = process.env.NODE_ENV === 'production'
-            ? 'https://swiftlead-backend-production-9ac7.up.railway.app'
+            ? 'https://swiftlead-backend-production-832b.up.railway.app'
             : 'http://localhost:5000';
 
         socket = io(apiUrl, {
